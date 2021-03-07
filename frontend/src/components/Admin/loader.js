649fe393012f02a29d1ca38942f8e8743b9a545a
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-03-07 18:54:53 +0530

    Update loader.js

--- frontend/src/components/Admin/loader.js
+++ frontend/src/components/Admin/loader.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import kuchhbhi from '../../images/imgpsh_fullsize_anim.gif';
 export default function Loader(){

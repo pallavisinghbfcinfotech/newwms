918fcab356f83abcd015b40eb851459a1ba1fbac
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-03-21 09:45:54 +0530

    Update Taxsavinginvest.js

--- frontend/src/components/Admin/Taxsavinginvest.js
+++ frontend/src/components/Admin/Taxsavinginvest.js
@@ -23,8 +23,7 @@ class Taxsavinginvest extends Component {
       defaultValue:"",
       year:'',
       mon:'',
-      options:'',
-      selectedOption: null,
+      searchname:[],
     };
   }
   onChanger1(e) {
@@ -55,15 +54,16 @@ class Taxsavinginvest extends Component {
      });
  }
   onChanger2(e) {
+    //$(".search-data").hide();
     this.setState({
       rvalue: e.target.value    
     });
     if(e.target.value === "no"){
       $.ajax({
-        url: "/api/getapplicant1",
+        url: "/api/getapplicant",
         type: "GET",
          success: function (res2) {
-          this.setState({ options: res2 });
+          this.setState({ searchname: res2 });
         }.bind(this),
         error: function(jqXHR) {
           console.log(jqXHR);          
@@ -71,12 +71,34 @@ class Taxsavinginvest extends Component {
       });
     }
   }
-  
-  changeApplicant = (selectedOption) =>{ 
-    this.setState({ selectedOption });
-     var optionElement = selectedOption.value
-     var name =  optionElement.split('/')[0];
-     var pan =  optionElement.split('/')[1];
+
+  suggestionBox = (e) =>{
+    $(".inputdata").show();
+    var inputValue = $(".searchname").val();
+    $.ajax({
+      url: "/api/getsearchname",
+      type: "POST",
+      data:{name: inputValue},
+      success: function (res4) {
+        this.setState({ searchname: res4 });        
+      }.bind(this),
+      error: function(jqXHR) {
+        console.log(jqXHR);         
+      }
+    });    
+}
+
+  changeApplicant = (e) =>{ 
+    $(".inputdata").hide();
+  $("#showmsg").hide();
+  $("#clientdata").hide();
+  $(".loader").css("display", "block");
+    $("#example1").css("display", "none");
+  var selectedvalue = e.target.innerText;
+  var name = selectedvalue.split('/')[0];
+  var pan = selectedvalue.split('/')[1];
+  $(".searchname").val(selectedvalue);
+  $(".inputdata").hide();
      this.setState({
       pan:pan,
       name: name
@@ -92,6 +114,13 @@ class Taxsavinginvest extends Component {
         this.setState({
           data3: res3.data,
           msg3: res3.message});
+          if(this.state.msg3 === "Data not found"){
+            $("#showmsg").html(this.state.msg3);
+            $("#showmsg").show();
+           
+           }
+          $(".loader").css("display", "none");
+          $("#example1").css("display", "block");
       }.bind(this),
       error: function(jqXHR) {
         console.log(jqXHR);          
@@ -121,6 +150,7 @@ changeyear = (e) =>{
       }
     });   
   }else{
+    $("#clientdata").hide();
     $.ajax({
       url: "/api/gettaxsavinguserwise",
       type: "POST",
@@ -129,13 +159,17 @@ changeyear = (e) =>{
         this.setState({
           data3: res3.data,
           msg3: res3.message});
+          if(res3.message === "Data not found"){
+            $("#showmsg").html(res3.message);
+            $("#clientdata").hide();
+           }
       }.bind(this)
     });
   }
 }
  
   componentDidMount(){
-    document.title = "WMS | Folio Detail"
+    document.title = "WMS | Tax Saving Investment Detail"
     var yer =  document.getElementById("finanyear").value
     var fromyear = yer.split('-')[0];
     var toyear = yer.split('-')[1];
@@ -153,10 +187,10 @@ changeyear = (e) =>{
       }
     });
     $.ajax({
-      url: "/api/getapplicant1",
+      url: "/api/getapplicant",
       type: "GET",
        success: function (res2) {
-        this.setState({ options: res2 });
+        this.setState({ searchname: res2 });
       }.bind(this),
       error: function(jqXHR) {
         console.log(jqXHR);          
@@ -165,7 +199,6 @@ changeyear = (e) =>{
   }
   
   render() {      
-    const { selectedOption,options } = this.state;
     return (  
       <>
       <style jsx>
@@ -182,6 +215,33 @@ changeyear = (e) =>{
         .table-fix-height{
           height:500px;
         }
+        .hide-bal{
+          display:none;
+        }
+        .search-data{
+          list-style:none;
+          padding:10px;
+          border:1px solid #eee;
+          height:auto;
+          overflow-y: auto;
+          background-color:white;
+          position:absolute !important;
+          width:auto;
+          min-width:1040px;
+          max-width:1040px;
+          max-height:200px;
+          z-index:9999;
+        }
+        .search-data li {
+          list-style: none;
+          padding: 6px 10px;
+          border-bottom: 1px solid #eee;
+          cursor:pointer;
+          
+        }
+          .search-data li:hover{
+            background-color:#eee;
+          }
         `}
         </style>
       <div className="content-wrapper">
@@ -244,14 +304,6 @@ changeyear = (e) =>{
                         </label>
                       </div>
                     </div>
-                        {/* <div className="form-check-inline">
-                          <input type="radio" id="yes" name="kstatus" defaultValue="yes" onClick={this.onChangekstatus}  />
-                          <label htmlFor="yes">ALL</label>&nbsp;   <br/>
-                        </div> */}
-                        {/* <div className="form-check-inline">
-                          <input type="radio" id="no" name="kstatus" defaultValue="no" onClick={this.onChangekstatus}/>
-                          <label htmlFor="no">Userwise</label>
-                        </div>  */}
                       </div>
                     </div>
                     <Loader/>
@@ -264,12 +316,7 @@ changeyear = (e) =>{
                         <div className="card-header bg-primary">
                           <h3 className="card-title"></h3>
                           <div className="card-tools">
-                            {/* <div className="input-group input-group-sm" style={{width: '150px'}}>
-                              <input type="text" name="table_search" className="form-control float-right" placeholder="Search" />
-                              <div className="input-group-append">
-                                <button type="submit" className="btn btn-default"><i className="fas fa-search" /></button>
-                              </div>
-                            </div> */}
+                          
                           </div>
                         </div>
                         {/* /.card-header */}
@@ -296,11 +343,6 @@ changeyear = (e) =>{
                                     <td>{item.SCHEME}</td>
                                     <td>{item.AMOUNT}</td>
                                     <td>{item.TRXN_NATURE}</td>
-                                    {/* {(item.TRXN_NATURE.match(new RegExp(`${"Systematic - To"}`))) ? (
-                                     <td>STP</td> ) :(item.TRXN_NATURE.match(new RegExp(`${"SIP Rejection"}`)) || item.TRXN_NATURE.match(new RegExp(`${"Systematic"}`))  && (Math.sign(item.AMOUNT) === -1)) ? (
-                                      <td>SIP Reversal</td>  ) :(item.TRXN_NATURE.match(new RegExp(`${"Purchase"}`))  ) ? (
-                                        <td>Purchase</td>  ) :(
-                                        <td>SIP</td> ) } */}
                                 </tr>
                             
                             ))}
@@ -327,18 +369,15 @@ changeyear = (e) =>{
                         <div className="col-md-12">
                          <div className="form-group">
                             <label>Applicant</label>
-                            {/* <select className="form-control" onChange={this.changeApplicant} id="user">
-                              <option>Select Applicant</option>
-                              {this.state.data2.map((item, index) => (
-                                      <option data-pan={item.PAN} data-name={item.INVNAME} value={item.PAN}>{item.INVNAME}/{item.PAN}</option>  
-                                ))}
-                             </select> */}
-
-<Select
-        value={selectedOption}
-        onChange={this.changeApplicant}
-        options={options} 
-      />
+                                      <input type="text" name="searchname" onKeyUp={this.suggestionBox} className="form-control searchname" autoComplete="off" />
+                                        <div className="inputdata">
+                                            <ul className="search-data">
+                                              {this.state.searchname.map((item, index) => (
+                                                <li onClick={this.changeApplicant} >{item.INVNAME}/{item.PAN}</li> 
+                                                ))}
+                                               
+                                            </ul>
+                                          </div>
                           </div>
                         </div>
                         </div>
@@ -347,17 +386,11 @@ changeyear = (e) =>{
                         <div> 
 			 
        { (this.state.msg3==='Successfull')? (
-                      <div className="card">
+                      <div id="clientdata" className="card">
                         <div className="card-header bg-primary">
                           <h3 className="card-title"></h3>
                           <div className="card-tools">
-                            {/* <div className="input-group input-group-sm" style={{width: '150px'}}>
-                              <input type="text" name="table_search" className="form-control float-right" placeholder="Search" />
-                              <div className="input-group-append">
-                                <button type="submit" className="btn btn-default"><i className="fas fa-search" /></button>
-                              </div>
-                            </div> */}
-                          </div>
+                            </div>
                         </div>
                         {/* /.card-header */}
                         <div className="card-body">
@@ -383,11 +416,6 @@ changeyear = (e) =>{
                                     <td>{item.SCHEME}</td>
                                     <td>{item.AMOUNT}</td>
                                     <td>{item.TRXN_NATURE}</td>
-                                    {/* {(item.TRXN_NATURE.match(new RegExp(`${"Systematic - To"}`))) ? (
-                                     <td>STP</td> ) :(item.TRXN_NATURE.match(new RegExp(`${"SIP Rejection"}`)) || item.TRXN_NATURE.match(new RegExp(`${"Systematic"}`))  && (Math.sign(item.AMOUNT) === -1)) ? (
-                                      <td>SIP Reversal</td>  ) :(item.TRXN_NATURE.match(new RegExp(`${"Purchase"}`))  ) ? (
-                                        <td>Purchase</td>  ) :(
-                                        <td>SIP</td> ) } */}
                                 </tr>
                             
                             ))}
@@ -401,7 +429,7 @@ changeyear = (e) =>{
                       </div>
                         ):  (<div align="center"  className="col-sm-10">
                           <br/>
-                        <h6>Data Not Found</h6>
+                          <h6 id="showmsg"></h6>
                       </div>)}
 			 
 	  </div>

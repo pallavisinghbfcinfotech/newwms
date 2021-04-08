38957525a46d587e82240e4156882c5a26334a04
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-04-08 18:07:29 +0530

    Update Dividend.js

--- frontend/src/components/Admin/Dividend.js
+++ frontend/src/components/Admin/Dividend.js
@@ -262,12 +262,19 @@ changeyear = (e) =>{
                                     <i className="far fa-calendar-alt" />
                                     </span>
                                 </div>
-                                <select className="form-control" onChange={this.changeyear}  id="finanyear" >
+                                  <select className="form-control" onChange={this.changeyear}  id="finanyear" >
                                 <option value="2020-2021">2020-2021</option>
                                 <option value="2019-2020">2019-2020</option> 
                                 <option value="2018-2019">2018-2019</option>
                                 <option value="2017-2018">2017-2018</option>
                                 <option value="2016-2017">2016-2017</option>
+                                <option value="2015-2016">2015-2016</option>
+                                <option value="2014-2015">2014-2015</option>
+                                <option value="2013-2014">2013-2014</option>
+                                <option value="2012-2013">2012-2013</option>
+                                <option value="2011-2012">2011-2012</option>
+                                <option value="2010-2011">2010-2011</option>
+                                <option value="2009-2010">2009-2010</option>
                               </select>    
                             </div>
                             {/* /.input group */}

a76b524941f3f53ddd2906c8e30dcde66f253b2a
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-04-08 22:02:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -267,6 +267,84 @@ app.get("/api/gettranscams", function (req, res) {
     });
 })
 
+app.post("/api/getschemedetail11", function (req, res) {
+const pipeline1 = [  //trans_karvy    
+    { $match: { FUNDDESC: req.body.scheme, PAN1: req.body.pan ,TD_ACNO:req.body.folio } },
+    { $project: { _id:1,PAN: "$PAN1", FUNDDESC:"$FUNDDESC",TD_NAV: "$TD_NAV", NATURE: "$TD_TRTYPE", TD_TRDT: { $dateToString: { format: "%d-%m-%Y", date: "$TD_TRDT" } }, TRDESC: "$TRDESC", INVNAME: "$INVNAME",  UNITS: { $sum: "$TD_UNITS" }, AMOUNT: { $sum: "$TD_AMT" } ,RTA:"KARVY" } },
+    {$sort : {TD_TRDT : -1}}
+]
+const pipeline2=[  //trans_franklin
+    {$match: { SCHEME_NA1: req.body.scheme, IT_PAN_NO1: req.body.pan ,FOLIO_NO:req.body.folio } },
+    { $project: { _id:1,PAN: "$IT_PAN_NO1", FUNDDESC:"$SCHEME_NA1",TD_NAV: "$NAV", NATURE: "$TRXN_TYPE", TD_TRDT: { $dateToString: { format: "%d-%m-%Y", date: "$TRXN_DATE" } }, TRDESC: "$TRXN_TYPE", INVNAME: "$INVESTOR_2",  UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } ,RTA:"FRANKLIN" } },
+    {$sort : {TD_TRDT : -1}}
+] 
+const pipeline3=[  //trans_cams
+    {$match: { SCHEME: req.body.scheme, PAN: req.body.pan ,FOLIO_NO:req.body.folio } },
+    { $project: { _id:1,PAN: "$PAN", FUNDDESC:"$SCHEME", TD_NAV: "NAV",NATURE: "$TRXN_TYPE_", TD_TRDT: { $dateToString: { format: "%d-%m-%Y", date: "$TRADDATE" } }, TRDESC: "$TRXN_TYPE_", INVNAME: "$INV_NAME",  UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } ,RTA:"CAMS" } },
+    {$sort : {TD_TRDT : -1}}
+] 
+
+transk.aggregate(pipeline1, (err, karvydata) => {
+    transf.aggregate(pipeline2, (err, frankdata) => {
+        transc.aggregate(pipeline3, (err, camsdata) => {
+    if (karvydata != 0 || frankdata !=0 || camsdata !=0) {
+        resdata = {
+            status: 200,
+            message: "Successfull",
+            data: karvydata
+        };
+    } else {
+        resdata = {
+            status: 400,
+            message: "Data not found"
+        };
+    }
+     var datacon = karvydata.concat(frankdata.concat(camsdata));    
+     for (var i = 0; i < datacon.length; i++) {
+        if (datacon[i]['NATURE'] === "Redemption" || datacon[i]['NATURE'] === "FUL") {
+            datacon[i]['NATURE'] = "RED";
+        }if (datacon[i]['NATURE'].match(/Systematic Investment.*/) || datacon[i]['NATURE'].match(/Systematic Withdrawal.*/) || datacon[i]['NATURE'].match(/Systematic - Instalment.*/) || datacon[i]['NATURE'].match(/Systematic - To.*/) || datacon[i]['NATURE'].match(/Systematic-NSE.*/) || datacon[i]['NATURE'].match(/Systematic Physical.*/) || datacon[i]['NATURE'].match(/Systematic.*/) || datacon[i]['NATURE'].match(/Systematic-Normal.*/) || datacon[i]['NATURE'].match(/Systematic (ECS).*/)) {
+            datacon[i]['NATURE'] = "SIP";
+        }
+        if (datacon[i]['NATURE'] === "Transmission Out" || datacon[i]['NATURE'] === "Switch Over Out" || datacon[i]['NATURE'] === "LTOP" || datacon[i]['NATURE'] === "LTOF" || datacon[i]['NATURE'] === "Lateral Shift Out") {
+            datacon[i]['NATURE'] = "Switch Out";
+        }
+        if ((Math.sign(datacon[i]['AMOUNT']) === -1) || datacon[i]['NATURE'] === "SINR") {
+            datacon[i]['NATURE'] = "SIP Rej.";
+        }
+        if (datacon[i]['NATURE'].match(/Systematic - From.*/)) {
+            datacon[i]['NATURE'] = "STP";
+        }
+        if (datacon[i]['NATURE'] === "Div. Reinvestment") {
+            datacon[i]['NATURE'] = "Div. Reinv.";
+        }
+        if (datacon[i]['NATURE'] === "Gross Dividend") {
+            datacon[i]['NATURE'] = "Gross Div.";
+        }
+        if (datacon[i]['NATURE'] === "Lateral Shift In" || 
+        datacon[i]['NATURE'] === "Switch Over In") {
+            datacon[i]['NATURE'] = "Switch In";
+        }
+        if (datacon[i]['NATURE'] === "Consolidation Out") {
+            datacon[i]['NATURE'] = "CNO";
+        }
+        if (datacon[i]['NATURE'] === "Consolidation In") {
+            datacon[i]['NATURE'] = "CNI";
+        }
+        if (datacon[i]['NATURE'] === "Purchase" || 
+        datacon[i]['NATURE'] === "NEW" || datacon[i]['NATURE'] === "Initial Allotment"
+        || datacon[i]['NATURE'] === "NEWPUR") {
+            datacon[i]['NATURE'] = "Purchase";
+        }
+    }        
+    resdata.data = datacon.sort((a, b) => new Date(a.TD_TRDT.split("-").reverse().join("/")).getTime() - new Date(b.TD_TRDT.split("-").reverse().join("/")).getTime())
+    res.json(resdata);
+    return resdata;
+});
+}); 
+})
+})
+
 app.post("/api/getsearchfoliodetail", function (req, res) {
                         var pipeline="";var trans='';var rta="";
                         if(req.body.rta === "CAMS"){

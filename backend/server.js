bc6901524202e84a7809f3ea99c98049041918b4
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-04-19 09:05:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2187,10 +2187,10 @@ app.post("/api/getportfolioscheme", function (req, res) {
 // })
 
 app.post("/api/getfoliodetailweb", function (req, res) {
-    var unit=0;var balance=0;var currentvalue=0;var amt=0;
+    var unit = 0; var balance = 0;var currentvalue=0;var amt=0;var cnav = 0;
     const pipeline1 = [  //trans_cams
-        { $match: { FOLIO_NO: req.body.folio, SCHEME: req.body.scheme } },
-        { $group: { _id: { FOLIO_NO:"$FOLIO_NO",INV_NAME: "$INV_NAME",SCHEME:"$SCHEME",BANK_NAME: "$BANK_NAME", AC_NO: "$AC_NO",TRXN_TYPE_: "$TRXN_TYPE_",AMC_CODE: "$AMC_CODE", PRODCODE: "$PRODCODE", code:{ $ltrim: { input: "$PRODCODE", chars: "$AMC_CODE" } } } , UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" }  } },
+        { $match: { FOLIO_NO: req.body.folio, SCHEME: req.body.scheme,PAN:req.body.pan } },
+        { $group: { _id: { FOLIO_NO:"$FOLIO_NO",INV_NAME: "$INV_NAME",SCHEME:"$SCHEME",BANK_NAME: "$BANK_NAME", AC_NO: "$AC_NO",TRXN_TYPE_: "$TRXN_TYPE_",TRADDATE:"$TRADDATE",AMC_CODE: "$AMC_CODE", PRODCODE: "$PRODCODE", code:{ $ltrim: { input: "$PRODCODE", chars: "$AMC_CODE" } } } , UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" }  } },
         {
             $lookup:
             {
@@ -2220,12 +2220,12 @@ app.post("/api/getfoliodetailweb", function (req, res) {
          { $unwind: "$nav" },
          { $lookup: { from: 'folio_cams', localField: '_id.FOLIO_NO', foreignField: 'FOLIOCHK', as: 'detail' } },
          { $unwind: "$detail" },
-         { $project: { _id: 0, FOLIO:"$_id.FOLIO_NO",INVNAME: "$_id.INV_NAME",SCHEME:"$_id.SCHEME",NATURE: "$_id.TRXN_TYPE_",ISIN: "$products.ISIN",NOMINEE: "$detail.NOM_NAME", BANK_NAME: "$_id.BANK_NAME", AC_NO: "$_id.AC_NO",JTNAME2: "$detail.JNT_NAME2", JTNAME1: "$detail.JNT_NAME1", cnav: "$nav.NetAssetValue",  UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } } },
+         { $project: { _id: 0, FOLIO:"$_id.FOLIO_NO",INVNAME: "$_id.INV_NAME",SCHEME:"$_id.SCHEME",NATURE: "$_id.TRXN_TYPE_",TD_TRDT:"$_id.TRADDATE",ISIN: "$products.ISIN",NOMINEE: "$detail.NOM_NAME", BANK_NAME: "$_id.BANK_NAME", AC_NO: "$_id.AC_NO",JTNAME2: "$detail.JNT_NAME2", JTNAME1: "$detail.JNT_NAME1", cnav: "$nav.NetAssetValue",  UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } } },
     ]
 
     const pipeline2 = [  //trans_karvy
-        { $match: { TD_ACNO: req.body.folio, FUNDDESC: req.body.scheme } },
-        { $group: { _id: {TD_ACNO:"$TD_ACNO", INVNAME: "$INVNAME",FUNDDESC:"$FUNDDESC",TD_TRTYPE: "$TD_TRTYPE", SCHEMEISIN: "$SCHEMEISIN" }, TD_UNITS: { $sum: "$TD_UNITS" } , TD_AMT: { $sum: "$TD_AMT" }} },
+        { $match: { TD_ACNO: req.body.folio, FUNDDESC: req.body.scheme ,PAN1:req.body.pan} },
+        { $group: { _id: {TD_ACNO:"$TD_ACNO", INVNAME: "$INVNAME",FUNDDESC:"$FUNDDESC",TD_TRTYPE: "$TD_TRTYPE",TD_TRDT:"$TD_TRDT", SCHEMEISIN: "$SCHEMEISIN" }, TD_UNITS: { $sum: "$TD_UNITS" } , TD_AMT: { $sum: "$TD_AMT" }} },
         {
             $lookup:
             {
@@ -2250,15 +2250,15 @@ app.post("/api/getfoliodetailweb", function (req, res) {
                 as: "nav"
             }
         },
-       // { $unwind: "$nav" },
+        { $unwind: "$nav" },
         { $lookup: { from: 'folio_karvy', localField: '_id.TD_ACNO', foreignField: 'ACNO', as: 'detail' } },
         { $unwind: "$detail" },
-        { $project: { _id: 0, FOLIO:"$_id.TD_ACNO",INVNAME: "$_id.INVNAME",SCHEME:"$_id.FUNDDESC",NATURE: "$_id.TD_TRTYPE",ISIN: "$_id.SCHEMEISIN" ,NOMINEE: "$detail.NOMINEE",  BANK_NAME: "$detail.BNAME" ,AC_NO: "$detail.BNKACNO", JTNAME2: "$detail.JTNAME2", JTNAME1: "$detail.JTNAME1",cnav: "$nav.NetAssetValue", UNITS: { $sum: "$TD_UNITS" }, AMOUNT: { $sum: "$TD_AMT" } } } ,
+        { $project: { _id: 0, FOLIO:"$_id.TD_ACNO",INVNAME: "$_id.INVNAME",SCHEME:"$_id.FUNDDESC",NATURE: "$_id.TD_TRTYPE",TD_TRDT:"$_id.TD_TRDT",ISIN: "$_id.SCHEMEISIN" ,NOMINEE: "$detail.NOMINEE",  BANK_NAME: "$detail.BNAME" ,AC_NO: "$detail.BNKACNO", JTNAME2: "$detail.JTNAME2", JTNAME1: "$detail.JTNAME1",cnav: "$nav.NetAssetValue", UNITS: { $sum: "$TD_UNITS" }, AMOUNT: { $sum: "$TD_AMT" } } } ,
     ]
 
     const pipeline3 = [  //trans_franklin
-        { $match: { FOLIO_NO: req.body.folio, SCHEME_NA1: req.body.scheme } },
-        { $group: { _id: { FOLIO_NO:"$FOLIO_NO", INVESTOR_2: "$INVESTOR_2",SCHEME_NA1:"$SCHEME_NA1",TRXN_TYPE: "$TRXN_TYPE", ISIN: "$ISIN", NOMINEE1: "$NOMINEE1", PBANK_NAME: "$PBANK_NAME", PERSONAL23: "$PERSONAL23", JOINT_NAM2: "$JOINT_NAM2", JOINT_NAM1: "$JOINT_NAM1" }, UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } } },
+        { $match: { FOLIO_NO: req.body.folio, SCHEME_NA1: req.body.scheme ,IT_PAN_NO1:req.body.pan} },
+        { $group: { _id: { FOLIO_NO:"$FOLIO_NO", INVESTOR_2: "$INVESTOR_2",SCHEME_NA1:"$SCHEME_NA1",TRXN_TYPE: "$TRXN_TYPE",TRXN_DATE:"$TRXN_DATE", ISIN: "$ISIN", NOMINEE1: "$NOMINEE1", PBANK_NAME: "$PBANK_NAME", PERSONAL23: "$PERSONAL23", JOINT_NAM2: "$JOINT_NAM2", JOINT_NAM1: "$JOINT_NAM1" }, UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } } },
        // { $lookup: { from: 'cams_nav', localField: '_id.ISIN', foreignField:{ $or: ['ISINDivReinvestment','ISINDivPayoutISINGrowth'] }, as: 'nav' } },
        // { $unwind: "$nav" },
        {
@@ -2286,17 +2286,17 @@ app.post("/api/getfoliodetailweb", function (req, res) {
         }
     },
     { $unwind: "$nav" },
-        { $project: { _id: 0,FOLIO:"$_id.FOLIO_NO", INVNAME: "$_id.INVESTOR_2",SCHEME:"$_id.SCHEME_NA1", NATURE: "$_id.TRXN_TYPE",ISIN: "$_id.ISIN", NOMINEE: "$_id.NOMINEE1", BANK_NAME: "$_id.PBANK_NAME", AC_NO: "$_id.PERSONAL23", JTNAME2: "$_id.JOINT_NAM2", JTNAME1: "$_id.JOINT_NAM1", cnav: "$nav.NetAssetValue", UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } } },
+        { $project: { _id: 0,FOLIO:"$_id.FOLIO_NO", INVNAME: "$_id.INVESTOR_2",SCHEME:"$_id.SCHEME_NA1", NATURE: "$_id.TRXN_TYPE",TD_TRDT:"$_id.TRXN_DATE",ISIN: "$_id.ISIN", NOMINEE: "$_id.NOMINEE1", BANK_NAME: "$_id.PBANK_NAME", AC_NO: "$_id.PERSONAL23", JTNAME2: "$_id.JOINT_NAM2", JTNAME1: "$_id.JOINT_NAM1", cnav: "$nav.NetAssetValue", UNITS: { $sum: "$UNITS" }, AMOUNT: { $sum: "$AMOUNT" } } },
     ]
 
        transc.aggregate(pipeline1, (err, camsdata) => {
           transk.aggregate(pipeline2, (err, karvydata) => {
               transf.aggregate(pipeline3, (err, frankdata) => {
                         if (frankdata != 0 || karvydata != 0 || camsdata != 0) {
-                            resdata = {
+                           resdata = {
                                 status: 200,
                                 message: "Successfull",
-                                data: frankdata
+                                data: karvydata
                             };
                         } else {
                             resdata = {
@@ -2304,7 +2304,8 @@ app.post("/api/getfoliodetailweb", function (req, res) {
                                 message: "Data not found"
                             };
                         }
-                    var datacon = frankdata.concat(karvydata.concat(camsdata));
+                      var datacon = frankdata.concat(karvydata.concat(camsdata));
+                 
                         datacon = datacon
                             .map(JSON.stringify)
                             .reverse() // convert to JSON string the array content, then reverse it (to check from end to begining)
@@ -2313,35 +2314,43 @@ app.post("/api/getfoliodetailweb", function (req, res) {
                             }) // check if there is any occurence of the item in whole array
                             .reverse()
                             .map(JSON.parse);
-                            datacon = Array.from(new Set(datacon));
-                        
-                        for (var i = 0; i < datacon.length; i++) {
-                            if (datacon[i]['NATURE'] === "RED" || datacon[i]['NATURE'] ==='FUL' ||
-                            datacon[i]['NATURE'] ==='LTOP' || datacon[i]['NATURE'] ==='LTOF' || 
-                            datacon[i]['NATURE'] ==='STPO' || datacon[i]['NATURE'] ==='IPOR') {
+                            resdata.data = datacon;
+                        //     console.log(datacon)
+                        //     console.log(datacon.length)
+                        for(i = 0; i<datacon.length; i++) {
+                            if (datacon[i]['NATURE'] === "RED" ||datacon[i]['NATURE'] === "LTOP" ||datacon[i]['NATURE'] === "Lateral Shift Out" || 
+                            datacon[i]['NATURE'] === "Switch Out" ||datacon[i]['NATURE'] === "IPOR" ||datacon[i]['NATURE'] === "LTOF" ||
+                            datacon[i]['NATURE'] === "FUL" ||datacon[i]['NATURE'] === "STPO" ||datacon[i]['NATURE'] === "CNO" ||
+                            datacon[i]['NATURE'] === "FULR" ||datacon[i]['NATURE'] === "Full Redemption" || datacon[i]['NATURE'] === "Partial Switch Out"
+                            || datacon[i]['NATURE'] === "Full Switch Out" || datacon[i]['NATURE'] === "Partial Redemption") {
                                 unit = "-"+datacon[i].UNITS;
                               }else{
                                 unit = datacon[i].UNITS;
                               }
-                             balance = parseFloat(unit)+parseFloat(balance) 
-                            datacon[i].UNITS = balance;
-                            datacon[i].AMOUNT = parseFloat(datacon[i].cnav)*parseFloat(balance) ;
-                            datacon[i].BANK_NAME =  datacon[0].BANK_NAME;
-                            datacon[i].INVNAME =  datacon[0].INVNAME;
-                            datacon[i].AC_NO =  datacon[0].AC_NO
-                            datacon[i].NOMINEE =  datacon[0].NOMINEE
-                            datacon[i].JTNAME2 =  datacon[0].JTNAME2
-                            datacon[i].JTNAME1 =  datacon[0].JTNAME1
+                         
+                             balance = parseFloat(unit)+parseFloat(balance) ;
+                             console.log(i,balance)
+                             cnav = datacon[i].cnav;
+                        
                         }
-                        var index = datacon.length -1 ;
-                        resdata.data = [datacon[index]];
-                       
+                        
+                        var index = datacon.length - 1 ;
+                        datacon[index].UNITS = balance;
+                        datacon[index].AMOUNT = parseFloat(cnav)*parseFloat(balance) ;
+                        datacon[index].BANK_NAME =  datacon[0].BANK_NAME;
+                        datacon[index].INVNAME =  datacon[0].INVNAME;
+                        datacon[index].AC_NO =  datacon[0].AC_NO;
+                        datacon[index].NOMINEE =  datacon[0].NOMINEE;
+                        datacon[index].JTNAME2 =  datacon[0].JTNAME2;
+                        datacon[index].JTNAME1 =  datacon[0].JTNAME1;
+                       resdata.data = [datacon[index]];
+                         
                         res.json(resdata);
                         return resdata;
 
                     });
                });
-            });
+           });
 })
 
 app.post("/api/getfoliodetail", function (req, res) {     

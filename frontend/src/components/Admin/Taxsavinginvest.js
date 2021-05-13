02e7048b860264ed1ffb935fe50fe45861c8f973
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-05-13 10:11:38 +0530

    Update Taxsavinginvest.js

--- frontend/src/components/Admin/Taxsavinginvest.js
+++ frontend/src/components/Admin/Taxsavinginvest.js
@@ -277,11 +277,13 @@ changeyear = (e) =>{
                                     </span>
                                 </div>
                                 <select className="form-control" onChange={this.changeyear}  id="finanyear" >
+	    			<option value="2021-2022">2021-2022</option>
                                 <option value="2020-2021">2020-2021</option>
                                 <option value="2019-2020">2019-2020</option> 
                                 <option value="2018-2019">2018-2019</option>
                                 <option value="2017-2018">2017-2018</option>
                                 <option value="2016-2017">2016-2017</option>
+	    			<option value="2015-2016">2015-2016</option>
                               </select>    
                             </div>
                             {/* /.input group */}

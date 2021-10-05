3afea382f24067763d20232ee1c681a5200cef4d
pallavisinghbfcinfotech <78799332+pallavisinghbfcinfotech@users.noreply.github.com>
2021-10-05 18:17:28 +0530

    Update Client-mapping.js

--- frontend/src/components/Admin/Client-mapping.js
+++ frontend/src/components/Admin/Client-mapping.js
@@ -1,10 +1,6 @@
-import React from "react";
+import React,{ Component } from "react";
 import $ from "jquery";
-import { dateFormat } from 'dateformat';
-import { Component } from "react"; 
 import {  MDBDataTableV5 } from 'mdbreact';
-import { Link } from 'react-router-dom';
-import moment from 'moment';
 import Loader from './loader';
 
 class Trans_Report extends Component {
